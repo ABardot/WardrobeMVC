493b51d712e6a7cd3ea5bf5f3be268c723bfbf69
ABardot <aabardot5@gmail.com>
2018-03-25 19:22:02 -0400

    Added the out button and over button and the switch/swap divs in the shopping thumbnail.

--- WardrobeProject9/WardrobeProject9/Scripts/script.js
+++ WardrobeProject9/WardrobeProject9/Scripts/script.js
@@ -16,3 +16,26 @@ function Greeting() {
     x.getElementsByTagName('h2')[0].innerText = 'Welcome to closet app ' + userResponse + '!';
 }
 
+function overBtn(x) {
+    x.style.backgroundColor = "#87CEFA";
+    x.style.color = "#D2691E";
+}
+
+
+function outBtn(x) {
+    x.style.backgroundColor = "grey";
+    x.style.color = "black";
+}
+
+function SwapDivsWithClick(div1, div2) {
+    d1 = document.getElementById(div1);
+    d2 = document.getElementById(div2);
+    if (d2.style.display == "none") {
+        d1.style.display = "none";
+        d2.style.display = "flex";
+    }
+    else {
+        d1.style.display = "flex";
+        d2.style.display = "none";
+    }
+}
