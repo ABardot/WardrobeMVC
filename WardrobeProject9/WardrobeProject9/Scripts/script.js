da6f1b8df16402cc67ec42917e71a9ebdac88f2f
ABardot <aabardot5@gmail.com>
2018-03-25 16:00:47 -0400

    Add the onmouseover for landing greeting index and a prompt for the user to add name.

--- WardrobeProject9/WardrobeProject9/Scripts/script.js
+++ WardrobeProject9/WardrobeProject9/Scripts/script.js
@@ -1,8 +1,18 @@
-﻿//alert('Hello');
-
-
-// Method for Date
-
+﻿
 function myEnterFunction() {
     document.getElementById("demo2").innerHTML = "Created by Adrien Bardot";
 }
+
+function myFunction() {
+    var x = document.getElementById("main");
+    x.getElementsByTagName("h2")[0].style.fontSize = "50px";
+    x.getElementsByTagName("h2")[0].style.color = "red";
+   
+}
+
+function Greeting() {
+    var x = document.getElementById("main");
+    var userResponse = prompt('Welcome to the closet app. What is your name?');
+    x.getElementsByTagName('h2')[0].innerText = 'Welcome to closet app ' + userResponse + '!';
+}
+
