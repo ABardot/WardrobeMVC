b30aaaa5aaf055eb2355424a36d85d6491010d61
ABardot <aabardot5@gmail.com>
2018-03-25 22:54:24 -0400

    Added no mouse hover over and out on index page.

--- WardrobeProject9/WardrobeProject9/Scripts/script.js
+++ WardrobeProject9/WardrobeProject9/Scripts/script.js
@@ -1,21 +1,25 @@
 ﻿
+// Footer hover name swap
 function myEnterFunction() {
     document.getElementById("demo2").innerHTML = "Created by Adrien Bardot";
 }
 
+// This changes the size and the color of the greet meesage
 function myFunction() {
     var x = document.getElementById("main");
     x.getElementsByTagName("h2")[0].style.fontSize = "50px";
-    x.getElementsByTagName("h2")[0].style.color = "red";
+    x.getElementsByTagName("h2")[0].style.color = "orange";
    
 }
 
+// This prompts the user to enter name
 function Greeting() {
     var x = document.getElementById("main");
     var userResponse = prompt('Welcome to the closet app. What is your name?');
     x.getElementsByTagName('h2')[0].innerText = 'Welcome to closet app ' + userResponse + '!';
 }
 
+// This is for the shopping btn hover over & out
 function overBtn(x) {
     x.style.backgroundColor = "#87CEFA";
     x.style.color = "#D2691E";
@@ -40,6 +44,7 @@ function SwapDivsWithClick(div1, div2) {
     }
 }
 
+// This will redirect the user to a 3 party site
 function Redirect(x) {
     var confirmButton = document.getElementById(x);
     var userResponse = confirm('You are about to leave this site. If you want to stay, please select cancel.');
@@ -51,26 +56,56 @@ function Redirect(x) {
     }
 }
 
+
 function GetVilla() {
     var villaArray = new Array(0);
-    var userResponse = confirm('Would you like to check the availability of a colors?');
+    var userResponse = confirm('Would you like to check the availability of a colors?'); // Prompt user
     while (userResponse) {
         var villa = prompt('Please enter the color');
         userResponse = confirm('Would you like to add another color?');
         villaArray.push(villa);
     }
-    var d = new Date();
-    var m = d.getMonth();
+    // I tried to filter but couldn't get it to work
+    var r = "red";
+    var y = "yellow";
+    var shirt1 = "button down";
+    var shirt2 = "tank-top";
+    var bot1 = "jeans";
+    var bot2 = "slacks";
+  
+
     var x = document.getElementById("villaswap");
-    if (m > 0 && m < 4) {
+    if (r && shirt1) {
         x.getElementsByTagName('p')[1].innerText = villaArray[0];
-    } else if (m > 3 && m < 7) {
+    } else if (r && shirt2) {
         x.getElementsByTagName('p')[1].innerText = villaArray[1];
-    } else if (m > 6 && m < 10) {
+    } else if (r && bot1) {
         x.getElementsByTagName('p')[1].innerText = villaArray[2];
-    } else if (m > 9 && m < 13) {
+    } else if (r && bot2) {
         x.getElementsByTagName('p')[1].innerText = villaArray[3];
-    } else {
-        x.getElementsByTagName('p')[1].innerText = 'Sorry we have no villas available';
+    }
+    else if (y && shirt1) {
+            x.getElementsByTagName('p')[1].innerText = villaArray[4];
+    }
+    else if (y && shirt2) {
+        x.getElementsByTagName('p')[1].innerText = villaArray[5];
+    }
+    else if (y && bot1) {
+        x.getElementsByTagName('p')[1].innerText = villaArray[5];
+    }
+    else if (y && bot2) {
+        x.getElementsByTagName('p')[1].innerText = villaArray[5];
+    }
+    else {
+        x.getElementsByTagName('p')[1].innerText = 'Sorry we have those items available';
     }
 }
+
+function changeText() {
+    var text = document.getElementById("para").style.fontSize = "15px";
+}
+
+function normaltext() {
+    var text = document.getElementById("para").style.fontSize = "25px";
+}
+
